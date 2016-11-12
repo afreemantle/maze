c8b6ee66ed0ea7d7f86d68c6ecfb80c4d93f85c4
Michelle Navarro <mn2614@columbia.edu>
2016-11-12 17:58:38 -0500

    Trying to add .native file for maze.ml complilation error

--- maze.ml
+++ maze.ml
@@ -1,17 +1,4 @@
-type action = AST | LLVM_IR | Compile | Help
-
-let _ = function 
-    let action = if Array.length Sys.argv >1 then
-        List.assoc Sys.argv.(1) [ ("-a", AST) (* Print the AST*)
-                                  ("-l", LLVM_IR) (* Generate LLVM, don't check*) 
-                                  ("-c", Compile) (*Generate and check LLVM IR *)
-                                  ("-h", Help) ](*The help function*) 
-    else Compile in 
-    let lexbuf = Lexing.from_channel stdin in 
-    let ast = Parser.program Scanner.token lexbuf in 
-    match action with 
-        Ast -> print_string (Ast.string_of_program ast)
-
+type action = AST | Help (*LLVM_IR | Compile*)
 
 let help_string = (
     "Usage: maze [option] <source files>\n" ^
@@ -19,7 +6,24 @@ let help_string = (
     "\t -c: Compile \n" ^ 
     "\t -l: Generate LLVM without checking" ^
     "\t -a: Print Abstract Syntax tree" 
-)    
+)
+
+let _ =  
+    let action = if Array.length Sys.argv > 1 then
+        List.assoc Sys.argv.(1) [ ("-a", AST); 
+                                  ("-h", Help) ] 
+                                 (*
+                                  ("-l", LLVM_IR)  Generate LLVM, don't check 
+                                  ("-c", Compile) Generate and check LLVM IR 
+                                 *) 
+    else Help in 
+    let lexbuf = Lexing.from_channel stdin in 
+    let ast = Parser.program Scanner.token lexbuf in 
+    match action with 
+        Ast -> print_string (Ast.string_of_program ast)
+        | Help -> print_string help_string
+
+
 
 
     
