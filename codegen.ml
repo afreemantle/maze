31aa2b08963e6ddf592326cf3715e0c02ab6b794
AFreemantle <asf2161@columbia.edu>
2016-11-12 15:15:51 -0500

    updating with each type from ast

--- codegen.ml
+++ codegen.ml
@@ -15,15 +15,15 @@ let named_values:(string, llvalue) Hashtbl.t = Hashtbl.create 40
 let integer_t = integer_type context;;
 let bool_t = i1_type context;;
 let char_t = i8_type context;;
-let float_t = double_type context;; 
+let float_t = float_type context;; 
 let void_t = void_type context;; 
-let null_t = const_null context;;  
+(* let null_t = const_null context;; *)  
 
 
 let rec codegen_expr = function 
-    | A.Int n -> ...
-    | A.Bool bl -> ...
-    | A.Char ch -> ...
-    | A.Float fl -> ...
-    | A.Void vd -> ...
-    | A.Null nl -> ...
+    | A.Int n -> integer_type n
+    | A.Bool bl -> i1_type bl
+    | A.Char ch -> i8_type ch
+    | A.Float fl -> float_type fl
+    | A.Void vd -> void_type vd
+    (* | A.Null nl -> ... *)
