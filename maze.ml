e67828f59f875aabe054c522d4ff85be7e4387f2
Michelle Navarro <mn2614@columbia.edu>
2016-11-17 14:37:15 -0500

    maze.ml changes

--- maze.ml
+++ maze.ml
@@ -8,20 +8,34 @@ let help_string = (
     "\t -a: Print Abstract Syntax tree" 
 )
 
+let bad = ("bad")
+
+(*
 let _ =  
     let action = if Array.length Sys.argv > 1 then
         List.assoc Sys.argv.(1) [ ("-a", Ast); 
                                   ("-h", Help) ] 
-                                 (*
+                                *) (*
                                   ("-l", LLVM_IR)  Generate LLVM, don't check 
                                   ("-c", Compile) Generate and check LLVM IR 
-                                 *) 
+                                 *)(* 
     else Help in 
     let lexbuf = Lexing.from_channel stdin in 
     let ast = Parser.program Scanner.token lexbuf in 
     match action with 
         Ast -> print_string (Ast.string_of_program ast)
         | Help -> print_string help_string
+*)
+
+let _  = 
+    let action, filename = 
+        if Array.length Sys.argv = 1 then Help, ""
+        else Ast, ""
+
+    in
+    match action with 
+        Help -> print_string help_string 
+      | Ast -> print_string bad
 
 
 
