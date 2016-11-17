70e34d0c3e01323eb35aacef1e5a667217c79c81
Michelle Navarro <mn2614@columbia.edu>
2016-11-17 18:56:42 -0500

    Adding option checking to maze.ml

--- maze.ml
+++ maze.ml
@@ -1,24 +1,32 @@
-type action = Ast | Help (*LLVM_IR | Compile*)
+type action = Ast | Help | Error (*LLVM_IR | Compile*)
 
 let help_string = (
-    "Usage: maze [option] <source files>\n" ^
+    "Usage: ./maze [option] <source file>\n" ^
+    "\n Valid Options: \n"^
     "\t -h: Help \n" ^
     "\t -c: Compile \n" ^ 
-    "\t -l: Generate LLVM without checking" ^
-    "\t -a: Print Abstract Syntax tree" 
+    "\t -l: Generate LLVM without checking \n" ^
+    "\t -a: Print Abstract Syntax tree \n" 
 )
 
-let bad = ("bad")
+let invalid_arg_string = ("Invalid Arguments")
 
-(*
+let check_option = function
+     "-h" -> Help, ""
+    | _ as s -> Error, ""
+
+let check_action = function  
+
+
 let _ =  
-    let action = if Array.length Sys.argv > 1 then
+    let action, filename  = 
+        if Array.length Sys.argv = 1 then Help, "" 
+        else if Array.length Sys.argv = 2 then check_option (Sys.argv.(1))
+        else if Array.l
+        else Error, ""
         List.assoc Sys.argv.(1) [ ("-a", Ast); 
                                   ("-h", Help) ] 
-                                *) (*
-                                  ("-l", LLVM_IR)  Generate LLVM, don't check 
-                                  ("-c", Compile) Generate and check LLVM IR 
-                                 *)(* 
+    (* 
     else Help in 
     let lexbuf = Lexing.from_channel stdin in 
     let ast = Parser.program Scanner.token lexbuf in 
@@ -27,15 +35,10 @@ let _ =
         | Help -> print_string help_string
 *)
 
-let _  = 
-    let action, filename = 
-        if Array.length Sys.argv = 1 then Help, ""
-        else Ast, ""
-
     in
     match action with 
         Help -> print_string help_string 
-      | Ast -> print_string bad
+      | Ast -> print_string invalid_arg_string
 
 
 
