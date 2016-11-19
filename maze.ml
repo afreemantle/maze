7e0d85d0c68313ad00588a6c49da0df9916f7e79
Michelle Navarro <mn2614@columbia.edu>
2016-11-18 22:25:45 -0500

    It compiles now

--- maze.ml
+++ maze.ml
@@ -10,24 +10,24 @@ let help_string = (
     "\t -a: Print Abstract Syntax tree \n" 
 )
 
-let invalid_arg_string = ("Invalid Arguments")
+let invalid_arg_string = ("Invalid Arguments\n")
 
-let ast_holder = ("Ast would be printing")
+let ast_holder = ("Ast would be printing\n")
 
 let check_option = function
      "-h" -> Help, ""
     | _ as s -> Error, ""
 
 let check_action = function  
-      "-h" -> Help, ""
-    | "-a" -> Ast, ""
-    | _ as s -> Error, ""
+      "-h" -> Help 
+    | "-a" -> Ast 
+    | _ as s -> Error
 
 let _ =  
     let action, filename  = 
         if Array.length Sys.argv = 1 then Help, "" 
         else if Array.length Sys.argv = 2 then check_option (Sys.argv.(1))
-        else if Array.length Sys.argv = 3 then check_action (Sys.argv.(1)), Sys.argv.(2)
+        else if Array.length Sys.argv = 3 then check_action Sys.argv.(1), Sys.argv.(2)
         else Error, ""
     (* 
     else Help in 
