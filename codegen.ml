3e4aff9b2bb90671900e43cdb478e876ad065a03
AFreemantle <asf2161@columbia.edu>
2016-11-20 13:56:26 -0500

    Llvm boiler plate + print function decleration

--- codegen.ml
+++ codegen.ml
@@ -1,29 +1,32 @@
-open Llvm
-
+(* Codegen for MAZE - based off of MicroC *)
 
 module L = Llvm
 module A = Ast
 
-exception Error of string
+module StringMap = Map.Make(String)
 
-let context = global_context ()
-let the_module  = create_module context "maze codegen"
-let builder = builder context
-let named_values:(string, llvalue) Hashtbl.t = Hashtbl.create 40
+let translate (globals, functions) = 
+    let context = L.global_context () in 
+    let the_module = L.create_module context "maze"
 
+    and i32_t = L.i32_type context      (* int *)
+    and i8_t = L.i8_type context        (* printf *)
+    and i1_t = L.i1_type context        (* bool *)
+    and void_t = L.void_type context in (* void *)
+    (* add our other types here *)
 
-let integer_t = integer_type context;;
-let bool_t = i1_type context;;
-let char_t = i8_type context;;
-let float_t = float_type context;; 
-let void_t = void_type context;; 
-(* let null_t = const_null context;; *)  
+    let ltype_of_type = function 
+        A.Int -> i32_t
+      | A.Bool -> i1_t
+      | A.Void -> void_t in
+      (* add our other types here *)
 
+    (* This is where global var func would go *)
 
-let rec codegen_expr = function 
-    | A.Int n -> integer_type n
-    | A.Bool bl -> i1_type bl
-    | A.Char ch -> i8_type ch
-    | A.Float fl -> float_type fl
-    | A.Void vd -> void_type vd
-    (* | A.Null nl -> ... *)
+    let printf_t = 
+        L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+    let printf_func = 
+        L.declare_function "print" printf_t the_module in
+
+    
+
