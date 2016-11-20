29a4edbd535265cbdb91534d88635e613d58ac3c
Alexander Brown <aab2212@columbia.edu>
2016-11-20 16:48:22 -0500

    yells at duplicate variables

--- analyzer2.ml
+++ analyzer2.ml
@@ -22,28 +22,26 @@ let check classes =
     | _ -> ()
   in
 
-  let make_str_list = function
+  (*let make_str_list = function
       Field(_, n) -> print_endline(n)
     | _ -> ()
-  in
+  in *)
+  let get_second = function
+      Field(t, n) -> n
+    | _ -> ""
+  in 
 
   let check_locals_lists someMethod =
-      List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someMethod.locals
+      List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someMethod.locals;
+      report_duplicate (fun n -> "duplicate variable " ^ n) (List.map get_second someMethod.locals);
   in
 
   let print_dname someClass =
-      print_endline ("hi");
       (*List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someClass.dbody.vdecls;*)
       (*List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someClass.dbody.methods;*)
       List.iter check_locals_lists someClass.dbody.methods;
-      List.iter make_str_list someClass.dbody.vdecls
   in
       (* List.iter report_duplicate (fun n -> "duplicate variable " ^ n ) *)
-
-  (*let make_str_list = function
-      Field(_, n) -> print_string(n)
-    | _ -> ()
-  in *)
   
 
   (*List.iter make_str_list classes;*)
