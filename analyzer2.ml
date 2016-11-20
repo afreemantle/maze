6e0304b8991a6aaccd204bc4ff782dda63aafc8b
Alexander Brown <aab2212@columbia.edu>
2016-11-20 16:59:12 -0500

    cleaned up, still doesn't catch void

--- analyzer2.ml
+++ analyzer2.ml
@@ -17,15 +17,11 @@ let check classes =
 
   (* Raise an exception if a given binding is to a void type *)
   let check_not_void exceptf = function
-      Field(t, n) -> print_endline (string_of_datatype t)
-      (*Field(Void, n) -> raise (Failure (exceptf n))  *)
+      Field(Void, n) -> raise (Failure (exceptf n))  
+    | Field(t, n) -> print_endline (string_of_datatype t)
     | _ -> ()
   in
 
-  (*let make_str_list = function
-      Field(_, n) -> print_endline(n)
-    | _ -> ()
-  in *)
   let get_second = function
       Field(t, n) -> n
     | _ -> ""
@@ -37,12 +33,7 @@ let check classes =
   in
 
   let print_dname someClass =
-      (*List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someClass.dbody.vdecls;*)
-      (*List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someClass.dbody.methods;*)
       List.iter check_locals_lists someClass.dbody.methods;
   in
-      (* List.iter report_duplicate (fun n -> "duplicate variable " ^ n ) *)
-  
 
-  (*List.iter make_str_list classes;*)
   List.iter print_dname classes
