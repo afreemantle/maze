cac1f679a4eeb57b3909b3a0533cc418fd2617e8
Alexander Brown <aab2212@columbia.edu>
2016-11-20 18:40:21 -0500

    Organized, commented variable verifier

--- analyzer2.ml
+++ analyzer2.ml
@@ -5,6 +5,7 @@ module StringMap = Map.Make(String)
 
 let check classes =
 
+  (* ------------------------- Verify variable section ------------------------- *)
 
   (* Raise an exception if the given list has a duplicate *)
   let report_duplicate exceptf list =
@@ -15,6 +16,7 @@ let check classes =
     in helper (List.sort compare list)
   in
 
+  (* Helper function for check_not_void *)
   let typ_of_datatype = function
       Arraytype(p, i) -> p
     | Datatype(p) -> p
@@ -28,17 +30,25 @@ let check classes =
     | Field(t, n) -> if typ_of_datatype t == Void then raise (Failure (exceptf n)) else (print_endline n)
   in
 
+  (* Grabs second element of Field (vdecl) *)
   let get_second = function
       Field(t, n) -> n
   in 
 
+  (* Checks that local method variables are neither null nor duplicates *)
   let check_locals_lists someMethod =
       List.iter (check_not_void (fun n -> "illegal void variable " ^ n)) someMethod.locals;
       report_duplicate (fun n -> "duplicate variable " ^ n) (List.map get_second someMethod.locals);
   in
 
+  (* Checks local variables for each method of a class argument *)
   let print_dname someClass =
       List.iter check_locals_lists someClass.dbody.methods;
   in
 
-  List.iter print_dname classes
+  List.iter print_dname classes;
+
+
+  (* ------------------------- Verify function section ------------------------- *)
+
+
